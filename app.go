276a412e788c8369c409d603a59cfd4571e8c013
naofumi-fujii <naofumi.fujii@gmail.com>
2019-10-02 14:34:20 +0900

    .

--- app.go
+++ app.go
@@ -3,12 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
 	fs := http.FileServer(http.Dir("static"))
-	http.Handle("/", fs)
 
 	log.Println("Listening...")
-	http.ListenAndServe(":3000", nil)
+	s := &http.Server{
+		Addr:           ":8080",
+		Handler:        fs,
+		ReadTimeout:    10 * time.Second,
+		WriteTimeout:   10 * time.Second,
+		MaxHeaderBytes: 1 << 20,
+	}
+	log.Fatal(s.ListenAndServe())
 }
